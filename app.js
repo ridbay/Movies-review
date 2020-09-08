5c5a26c125b154c3602f1f7237a14ab33e8a1b91
Ridwan Balogun <balogunridwan@gmail.com>
2020-09-08 21:11:39 +0100

    Added MongoDB and review model

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
+const mongoose = require("mongoose");
 const express = require("express");
 
 const app = express();
+mongoose.connect("mongodb://localhost/movie-reviews", {
+  useNewUrlParser: true,
+});
+const Review = mongoose.model("Review", {
+  title: String,
+  movieTitle: String,
+});
+
 const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // MOCK ARRAY OF PROJECTS
-let reviews = [
-    { title: "Great Review", movieTitle: "Batman II" },
-    { title: "Awesome Movie", movieTitle: "Titanic" }
-  ]
+// let reviews = [
+//     { title: "Great Review", movieTitle: "Batman II" },
+//     { title: "Awesome Movie", movieTitle: "Titanic" }
+//   ]
 
 app.get("/", (req, res) => {
-  res.render('reviews-index', {reviews: reviews});
+  Review.find()
+    .then((reviews) => {
+      res.render("reviews-index", { reviews: reviews });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 const PORT = 4000;
