203e307a5a62e78ad30ea870ad840f91f6bcab39
Ridwan Balogun <balogunridwan@gmail.com>
2020-09-11 11:42:36 +0100

    Users can add and see single reviews

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const mongoose = require("mongoose");
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const Handlebars = require('handlebars')
+const expressHandlebars = require('express-handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+ 
 const app = express();
 mongoose.connect("mongodb://localhost/movie-reviews", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
 const Review = mongoose.model("Review", {
   title: String,
   movieTitle: String,
+  description: String,
 });
 
-const exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine('handlebars', expressHandlebars({
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+}));
+app.set('view engine', 'handlebars');
 
 // MOCK ARRAY OF PROJECTS
 // let reviews = [
@@ -22,7 +30,8 @@ app.set("view engine", "handlebars");
 //   ]
 
 app.get("/", (req, res) => {
-  Review.find()
+  Review.find({})
+    .lean()
     .then((reviews) => {
       res.render("reviews-index", { reviews: reviews });
     })
@@ -31,6 +40,28 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get("/reviews/new", (req, res) => {
+  res.render("reviews-new", {});
+});
+
+app.post("/reviews", (req, res) => {
+  Review.create(req.body)
+    .then((review) => {
+      res.redirect(`/reviews/${review._id}`);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+app.get("/reviews/:id", (req,res)=>{
+    Review.findById(req.params.id)
+    .then((review)=>{
+        res.render('reviews-show', {review: review})
+    }).catch(err=>{
+        console.log(err.message)
+    })
+})
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
